e78d5d882cf89e322e3eb621fb88c2bafb0ab811
alwalker21@live.com <alwalker@seven-laptop.(none)>
2012-04-17 07:52:17 -0500

    Added F# Windows Service POC.

---
+++ FSharp_WinService_POC/FSharp_WinService_POC/Main.fs
@@ -0,0 +1,10 @@
+﻿namespace ServiceLayer
+open System.ServiceProcess
+
+//This has to be in a seperate file!
+module Main = 
+    [<EntryPoint>] 
+    let Main(args) = 
+        ServiceBase.Run(new WindowsService())
+        0
+        
---
+++ FSharp_WinService_POC/FSharp_WinService_POC/Service.fs
@@ -0,0 +1,39 @@
+﻿namespace ServiceLayer
+    open System.ComponentModel 
+    open System.Configuration.Install 
+    open System.ServiceProcess
+    open System.IO
+    
+    type WindowsService() as this = 
+        inherit ServiceBase()
+
+        do 
+            this.ServiceName <- "Name" 
+            this.EventLog.Log <- "Application"
+            
+        override this.OnStart(args:string[]) = 
+            base.OnStart(args)
+            let file = new FileInfo @"C:\test.txt"
+            use stream = file.CreateText()
+            stream.WriteLine("Test!")
+            stream.Close()
+            
+        override this.OnStop() = 
+            base.OnStop()            
+
+    [<RunInstaller(true)>] 
+    type MyInstaller() as this = 
+        inherit Installer() 
+        do 
+            let spi = new ServiceProcessInstaller() 
+            let si = new ServiceInstaller() 
+            spi.Account <- ServiceAccount.LocalSystem 
+//            spi.Username <- "UserName" 
+//            spi.Password <- "Password"
+
+            si.DisplayName <- "Fance Name"
+            si.StartType <- ServiceStartMode.Automatic 
+            si.ServiceName <- "Fancy Name"
+
+            this.Installers.Add(spi) |> ignore 
+            this.Installers.Add(si) |> ignore
