ac8e506201576987147c95812b207df1dd23dfb9
alwalker21@live.com <alwalker@seven-laptop.(none)>
2012-04-17 07:38:47 -0500

    Added silverlight rx poc. Can't figure out subscribe.

---
+++ Silverlight_Rx_POC/Silverlight_Rx_POC/CrazyEventClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Silverlight_Rx_POC
+{
+    public class CrazyEventClass
+    {
+        public delegate void MyEventHandler();
+        private event MyEventHandler _myEventCompleted;
+        public event MyEventHandler MyEvent
+        {
+            add { _myEventCompleted += value; }
+            remove { _myEventCompleted -= value; }
+        }
+
+        public delegate void SonOfMyEventHandler(EventArgs e);
+        private event SonOfMyEventHandler _sonOfMyEventCompleted;
+        public event SonOfMyEventHandler SonOfMyEvent
+        {
+            add { _sonOfMyEventCompleted += value; }
+            remove { _sonOfMyEventCompleted -= value; }
+        }
+
+        public delegate void MyEventvsKingKongHandler();
+        private event MyEventvsKingKongHandler _myEventvsKingKongCompleted;
+        public event MyEventvsKingKongHandler MyEventVSKingKong
+        {
+            add { _myEventvsKingKongCompleted += value; }
+            remove { _myEventvsKingKongCompleted -= value; }
+        }
+
+        public delegate void MyEventvsMothraHandler();
+        private event MyEventvsMothraHandler _myEventvsMothraCompleted;
+        public event MyEventvsMothraHandler MyEventVSMothra
+        {
+            add { _myEventvsMothraCompleted += value; }
+            remove { _myEventvsMothraCompleted -= value; }
+        }
+    }
+}
---
+++ Silverlight_Rx_POC/Silverlight_Rx_POC/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Reactive.Linq;
+using System.Reactive;
+
+namespace Silverlight_Rx_POC
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoIt_Click(object sender, RoutedEventArgs e)
+        {
+            var ec = new CrazyEventClass();
+
+            var myevent = Observable.FromEvent<CrazyEventClass.MyEventHandler, EventArgs>(h => ec.MyEvent += h, h => ec.MyEvent -= h);
+            //var sonofmyevent = Observable.FromEvent<CrazyEventClass.SonOfMyEventHandler>(h => ec.SonOfMyEvent += h, h => ec.SonOfMyEvent -= h);
+            //var myeventvskingkong = Observable.FromEvent<CrazyEventClass.MyEventvsKingKongHandler>(h => ec.MyEventVSKingKong += h, h => ec.MyEventVSKingKong -= h);
+            //var myeventvsmothra = Observable.FromEvent<CrazyEventClass.MyEventvsMothraHandler>(h => ec.MyEventVSMothra += h, h => ec.MyEventVSMothra -= h);
+
+            //var merged = Observable.Merge(
+            //    myevent.Select(_ => Unit.Default),
+            //    sonofmyevent.Select(_ => Unit.Default),
+            //    myeventvskingkong.Select(_ => Unit.Default),
+            //    myeventvsmothra.Select(_ => Unit.Default)
+            //    );
+            //merged.Subscribe(_ => { MessageBox.Show("DidIt"); });
+
+            myevent.Subscribe(_ => MessageBox.Show("Did it"));
+        }
+
+        private void DidIt()
+        {
+            MessageBox.Show("Did it");
+        }
+    }
+}
