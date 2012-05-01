5a4e5376c9a587289d2637636c87b73b575f5b53
Andrew Walker <alwalker21@live.com>
2012-05-01 18:32:49 -0500

    More F#/SL/Odata madness.

---
+++ SilverlightOdataFSharpPOC/SilverlightOdataFSharpPOC.Models/PayGrade.fs
@@ -0,0 +1,11 @@
+﻿module PayGrade
+
+open System
+open SilverlightOdataFSharpPOC.Stuff.DataFeedServiceReference
+
+let GetAllPayGrades =
+    async {
+        let context = new POCModel(new Uri "blargity blarg")
+        let! res = context.BeginExecute null, null, null
+        res
+    }
---
+++ SilverlightOdataFSharpPOC/SilverlightOdataFSharpPOC.Odata/DataFeed.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using SilverlightOdataFSharpPOC.Data;
+
+namespace SilverlightOdataFSharpPOC.Odata
+{
+    public class DataFeed : DataService<POCModel>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("PayGrades", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
